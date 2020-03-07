f48062f3e39d349755939c99f283fcad485b38c3
EvertonAvelino <everton-vitor@hotmail.com>
2020-03-07 11:25:22 -0300

    refazendo projeto mvc

--- app.js
+++ app.js
@@ -5,13 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app/views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
