2afc9b4e0472ca8367c7460de81749505d5bc589
EvertonAvelino <everton-vitor@hotmail.com>
2020-03-18 13:19:19 -0300

    finalizando a aula 05

--- app/controllers/adm_controller.js
+++ app/controllers/adm_controller.js
@@ -1,14 +1,30 @@
 var Adm = require("../models/adm");
+
 const AdmController = {
-  /*
+
+        index:(req, res, next) => {
+
+          Adm.find().then(dado  =>res.send(dado));
+/*          
+          const adm= new Adm({nome:'Suporte', senha:'654321', email:'suporte@everton.com.br'});
+          adm.save(error =>{
+            if(error){
+              res.send(error);
+              return
+            }
+            Adm.find().then(dado=>{
+              res.send(dado);
+            });
+          });
+ */         
+        }        
+ }
+ module.exports = AdmController; 
+
+   /*
     index: function(req, res, next) {
          res.send(Adm.todos());
        }
  */
-   // outra forma de fazer a função javascript
-        index:(req, res, next) => {
-          res.send(Adm.todos());
-       }
-     
- }
- module.exports = AdmController; 
+
+// outra forma de fazer a função javascript
--- app/models/adm.js
+++ app/models/adm.js
@@ -1,4 +1,5 @@
 var mongoose = require("../../db/conexao");
+
 const Adm = mongoose.model('user',{
     nome: { type:String, required:true},
     email: { type:String, required:true, unique:true},
