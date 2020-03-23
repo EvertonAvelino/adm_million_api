834f657afa08e768a50226a5d3d19f2cc22a34fd
EvertonAvelino <everton-vitor@hotmail.com>
2020-03-23 11:21:46 -0300

    aula 06-> implementando teste de alteração de nome

--- spec/models/adm_spec.js
+++ spec/models/adm_spec.js
@@ -15,6 +15,44 @@ describe('Modelo Adm', () => {
         expect(error == undefined || error == null).toBe(true);
       });
     });
+
+    it(' não deve incluir um admnistrador repetido', () => {
+      let nome = "teste" + new Date().getTime();
+      const adm= new Adm({nome: nome, senha:'654321', email: nome + '@everton.com.br'});
+      adm.save(error =>{
+        const adm2= new Adm({nome: nome, senha:'654321', email: nome + '@everton.com.br'});
+        adm2.save(error =>{
+          expect(error == undefined || error == null).toBe(false);
+        });
+      });
+    });
+
+
+    it('nao deve incluir um admnistrador sem nome', () => {
+      let nome = "teste" + new Date().getTime();
+      const adm= new Adm({nome: null, senha:'654321', email: nome + '@everton.com.br'});
+      adm.save(error =>{
+        expect(error == undefined || error == null).toBe(false);
+      });
+    }); 
+    
+    
+    it(' não deve alterar nome', () => {
+      let nome = "teste" + new Date().getTime();
+      const adm= new Adm({nome: nome, senha:'654321', email: nome + '@everton.com.br'});
+      adm.save(error =>{
+        const adm= new Adm({nome: nome, senha:'654321', email: nome + '@everton.com.br'});
+        let novoNome = "Everton vitor" + new Date().getTime();
+        adm.nome = novoNome;
+        adm.save(error =>{
+          expect(error == undefined || error == null).toBe(false);
+          adm.find({nome:novoNome}).then(dado =>{
+            expect(error == undefined || error == null).toBe(false);
+          });
+        });
+      });
+    });
+
 });
 
 //nova forma de concatenar do javascript
